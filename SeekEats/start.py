b73b0038b17e32b79ec0b10613942a8bfb8b0ac4
Raymond Chen <zhenyechen@yahoo.com>
2018-03-14 06:00:22 -0400

    test

--- SeekEats/start.py
+++ SeekEats/start.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name ='sushi.jpg'
+file_name ='iphone.jpg'
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
